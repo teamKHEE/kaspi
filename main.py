599b92a884931d3fe21c39dce38531b4aec59d85
yernazarius <72621687+yernazarius@users.noreply.github.com>
2023-02-24 20:12:24 +0600

    Update main.py
    успешный успех;
    01.1

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-import fake_useragent
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import time
 
-
-from requests_html import HTMLSession
-
-
+import fake_useragent
 import requests
 
 
 class Main:
     def __init__(self) -> None:
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, 10)
         self.url_forNames = 'https://kaspi.kz/shop/nur-sultan/c/categories/?page='
         self.user = fake_useragent.UserAgent().random
         self.header = {'user-agent': self.user}
-        self.boooo = False
 
     def parseTheLinkForYernazar(self, item):
         link = item.find('a', 'item-card__name').get('href')
         self.driver.get(link)
         self.driver.implicitly_wait(10)
-        if (self.boooo == False):
-            self.wait.until(EC.element_to_be_clickable((
-                By.XPATH, '/html/body/div[3]/div[1]/div/div[1]/div[2]'))).click()
-            self.boooo = True
-        soup = BeautifulSoup(self.driver.page_source,
-                             'lxml')
-        try:  # Selenium usage
+
+        soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        try:
             table = soup.find('td', class_='sellers-table__cell')
             link_item = table.find('a').get('href')
             return link_item
